94679dca0b396cc38b4c5d2bce1243d59e3b5c96
albafontcuadras <134973372+albafontcuadras@users.noreply.github.com>
2026-02-04 14:56:19 +0100

    Delete npass_site directory

--- npass_site/npass/ml.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def predict(features: dict):
-    # TEMP: return a dummy NPASS-like score in [-1, 1]
-    return round(random.uniform(-1, 1), 3)
--- npass_site/npass/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, predict_view
-
-urlpatterns = [
-    path("", home, name="home"),
-    path("api/predict", predict_view, name="predict"),
-]
